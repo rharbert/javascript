605ad18484476f0be0fec64bc7ccb199b127dcba
Rachael <rachael@bodigiti.com>
2020-07-15 09:39:03 -0400

    Updated fcc-diff-two-arrays lesson with Alternate solution

--- fcc-diff-two-arrays.js
+++ fcc-diff-two-arrays.js
@@ -10,10 +10,21 @@ function diffArray(arr1, arr2) {
     var newArr = arr1.concat(arr2);
     // console.log(newArr); //For verification only
     return newArr.filter(element => !arr1.includes(element) || !arr2.includes(element));
-  }
+}
   
-var result = diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+var result = diffArray([1, 2, 3, 5, 6], [1, 2, 3, 4, 5]);
 console.log(result);
-  
 
-//How else could this be done per Nils?
+//Alternate solution using .forEach and ternary operator...
+function diffArray2(arr1, arr2) {
+  var newArr = arr1.concat(arr2);
+  var notInBothArrays = [];
+
+  newArr.forEach(e => {
+    !arr1.includes(e) || !arr2.includes(e) ? notInBothArrays.push(e) : null;
+  })
+  return notInBothArrays;
+}
+
+var result2 = diffArray2([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(result2);
