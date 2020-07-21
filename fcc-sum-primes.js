a8da2d4b733f6f244937c47f4bd0a6e134098780
Rachael <rachael@bodigiti.com>
2020-07-21 12:53:42 -0400

    Added FCC lesson for Sum All Primes

--- fcc-sum-primes.js
+++ fcc-sum-primes.js
@@ -7,37 +7,7 @@ divisible by 1, 2 and 4.
 Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
 */
 
-// function sumPrimes(num) {
-//     var answer = 0;
-  
-//     //loop through all numbers from between 2 and num
-  
-//     for(var i=2; i <= num; i++) {   
-  
-//       //sum only prime numbers, skip all others
-//       if(num % i === 0){
-//         answer += i;
-//       }
-//     }
-//     return answer;
-//   }
-  
-//   var result = sumPrimes(10); // 73156
-//   console.log(result);
-
-
-//Shouldn't isPrime be inside sumPrimes? If so, how to do it properly???
 function sumPrimes(num) {
-    let i = 1;
-    let sum = 0;
-    while (i <= num) {
-      if (isPrime(i)) {
-        sum += i;
-      }
-      i++;
-    }
-    return sum;
-  }
   //function to check if a number is prime or not
   function isPrime(x) {
     for (let i = 2; i < x; i++) {
@@ -45,9 +15,17 @@ function sumPrimes(num) {
     }    
     return (x !== 1) && (x !== 0);
   }
-  
-  var result = sumPrimes(10);
-  console.log(result);
 
-  let test = 2 !== 1 && 4 !== 4;
-  console.log("test", test);
+  let i = 2;
+  let sum = 0;
+  while (i <= num) {
+    if (isPrime(i)) {
+      sum = sum + i;
+    }
+    i++;
+  }
+  return sum;
+}
+  
+var result = sumPrimes(20);
+console.log(result);
