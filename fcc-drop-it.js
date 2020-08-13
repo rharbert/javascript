30d41ebc414caea95707541d0353449b01160994
Rachael <rachael@bodigiti.com>
2020-08-13 10:51:21 -0400

    Added FCC 'Drop It' lesson

--- fcc-drop-it.js
+++ fcc-drop-it.js
@@ -1,25 +1,24 @@
 /*
 Intermediate Algorithm Scripting: Drop it
 
-Given the array arr, iterate through and remove each element starting from the first element (the 0 index)
-until the function func returns true when the iterated element is passed through it.
+Given the array arr, iterate through and remove each element starting from the first element (the
+0 index) until the function func returns true when the iterated element is passed through it.
 
-Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an
-empty array.
+Then return the rest of the array once the condition is satisfied, otherwise, arr should be 
+returned as an empty array.
 */
 
 function dropElements(arr, func) {
-
-  for (var i = 0; i <= arr.length; i++) {
-    if (func(arr[i]) === true) {
-			break;
-    } else {
-			arr.shift();
-    }
+  /* shift() method removes an item from the beginning of an array (aka index position 0).
+  So, we use while loop to test the beginning position of the array, and if it doesn't pass
+  the function, it is dropped. So, the next position in the array now "shifts" to index position 0,
+  and it's tested. Once the test passes, the rest of the array is returned per the instructions. */
+  while (!func(arr[0])) {
+    arr.shift();
   }
-	return arr;
+  return arr;
 }
 
-var result = dropElements([1, 2, 3, 4], function(n) {return n >= 3; });
+var result = dropElements([1, 2, 3, 4, 5, 1], function(n) {return n >= 3; });
 
-console.log(result); // Why is it including "2" ?????????????????????
+console.log(result); 
